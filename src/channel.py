c71241067ae7c0bcef9ccd7241314894aa95fe1a
Sudarev_D <89365265993@mail.ru>
2023-06-25 19:51:36 +0700

    channel.py is modified

--- src/channel.py
+++ src/channel.py
@@ -9,11 +9,51 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
         self.api_key = os.getenv('YT_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.subscribers_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.url = 'https://www.youtube.com/channel/' + self._channel_id
+
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    @property
+    def channel_id(self):
+        return self._channel_id
+
+    def to_json(self, filename):
+        data = {}
+        data['properties'] = []
+        data['properties'].append({
+            'channel_id': self._channel_id,
+            'title': self.title,
+            'description': self.description,
+            'subscribers_count': self.subscribers_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count,
+            'url': self.url})
+        with open(filename, 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile)
+
+
+
+
+
+
+
